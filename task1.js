28a1de0a41dc0dfd536e9016cb17ae9675d00c25
Vladislav <efremenko.exceedteam@gmail.com>
2021-10-01 11:52:59 +0300

    fix any bags

--- task1.js
+++ task1.js
@@ -7,19 +7,38 @@ let myMenu = document.getElementById("dropdown");
 
 myMenu.onchange = function() {
    let elementId = myMenu;
-   let valueElement = elementId.value
-   putChar(valueElement)}
+   let valueElement = elementId.value;
+   putChar(valueElement)};
 
+/////////////////////////////////////////////////////////////
+
+document.getElementById("input")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("submit").click();
+    }
+});
+
+/////////////////////////////////////////////////////////////
 
  function putChar (data) {
-     let myForm = document.getElementById("input")
-     myForm.value = data}
+     let myForm = document.getElementById("input");
+     myForm.value = data
+  };
 
 
 submit.onclick = function() {
 
-    let textInput = document.getElementById("input").value;
-    myMenu.options[myMenu.selectedIndex].text = textInput};
+    let textInput = (document.getElementById("input").value).replace(/\s+/g, '');
+    putChar(textInput)
 
 
+    if (textInput) {
+
+    myMenu.options[myMenu.selectedIndex].text = textInput;
+    } else {
+      alert("Incorrect Data")
+    }
+};
 
